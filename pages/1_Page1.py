9001bad1eabb8d6ddaceccebdda1cd6a518f74c1
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-09 02:04:57 -0600

    Update 1_Page1.py

--- pages/1_Page1.py
+++ pages/1_Page1.py
@@ -1,4 +1,3 @@
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs:
-  (["example 1-simple", "example 2-intermediate", "example 3-complex"], width 200)
+tab1, tab2, tab3 = st.tabs(["example 1-simple", "example 2-intermediate", "example 3-complex"], width 200)
