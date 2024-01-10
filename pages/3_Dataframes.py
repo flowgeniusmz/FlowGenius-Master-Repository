bdb46f4504dff10d963ab19f0b407ba7ad2d792e
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-09 23:05:36 -0600

    Update 3_Dataframes.py

--- pages/3_Dataframes.py
+++ pages/3_Dataframes.py
@@ -1,3 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from config import pagesetup as ps
+
+ps.set_title("FlowGenius", "Dataframes")
+
+tab_df_normal, tab_df_filtered = st.tabs(["Normal Dataframe", "Fitlered Dataframe"])
+
+with tab_df_normal:
+  ps.set_page_overview("Overview", "This section displays a basic dataframe using Pandas library. Dataframes can be created in many different ways - but displaying them is easy.")
+  exp1 = st.expander("Example Dataframe", expanded = True)
+  with exp1:
+    with st.echo():
+      df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+      st.dataframe(df)  # Same as st.write(df)
+  
+                       
+with tab_df_filtered:
+  exp2 = st.expander("Example Dataframe", expanded = True)
+  with exp2:
+    with st.echo():
+      df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+      st.dataframe(df)  # Same as st.write(df)
