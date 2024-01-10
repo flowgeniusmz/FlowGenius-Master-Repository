7b07d81be13bfd72a133bdc828429e3e62af6ae5
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-09 22:15:40 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,3 +7,4 @@ st.write('Description of what it does and how it does it (if that applies)')
 with st.expander("actual item displayed", expanded=True):
   st.write('visual of the item you are exploring')
 st.write('code provided below displayed example')
+st.subheader('*for all items you can create tabs that expand on the item described or present example with higher and higher complexity. Whatever you can throw at it is great, then we have more examples and a deeper understanding when we utilize it!')
