d1aafabb37477eb801c46f7c0f21e0d61cdd71b3
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-09 22:42:53 -0600

    Update 2_Page_Setup.py

--- pages/2_Page_Setup.py
+++ pages/2_Page_Setup.py
@@ -3,6 +3,21 @@ from config import pagesetup as ps
 
 #0. Set Title
 ps.set_title("FlowGenius", "Page Setup")
+st.echo()
+
+#1. Set the Page Overview
+ps.set_page_overview("Overview", "This page illustrates the page setup functions available for consistency across pages")
+
+#2. Container 1
+container1 = st.container()
+with container1:
+  ps.set_blue_header("This is section 1")
+
+  cc = st.columns(2)
+  with cc[1]:
+    ps.set_green_header("This is section 1.1")
+  with cc[2]:
+    ps.set_green_header("This is section 1.2")
 
 
 
