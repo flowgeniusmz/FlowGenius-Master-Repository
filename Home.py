7959a4d5efc674dd6af18372437518088ae1c0fa
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-09 22:11:25 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.header('formatting for all items in library:')
 st.subheader('name of item')
 st.write('Description of what it does and how it does it (if that applies)')
-with st.expander("actual item deisplayed", expanded=True):
+with st.expander("actual item displayed", expanded=True):
   st.write('visual of the item you are exploring')
 st.write('code provided below displayed example')
