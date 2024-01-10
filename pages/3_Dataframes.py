247ea5a40d25c8f3f1c56bf3624b64c54638d6a5
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-01-10 14:07:29 -0600

    Update 3_Dataframes.py

--- pages/3_Dataframes.py
+++ pages/3_Dataframes.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 from config import pagesetup as ps
 from functions import filter_dataframe as dfFilter
+from streamlit_condition_tree import condition_tree, config_from_dataframe
 
 
 
 
 # Set the title and page overview (assuming you have these functions in your 'ps' module)
 ps.set_title("FlowGenius", "Dataframes")
-tab_df_normal, tab_df_filtered, tab_df_filtered2 = st.tabs(["Normal Dataframe", "Filtered Dataframe", "Filtered Dataframe with Text"])
+tab_df_normal, tab_df_filtered, tab_df_filtered2, tab_df_conditiontree = st.tabs(["Normal Dataframe", "Filtered Dataframe", "Filtered Dataframe with Text", "Dataframe with Condition Tree"])
 
 with tab_df_normal:
     ps.set_page_overview("Overview", "This section displays a basic dataframe using Pandas library.")
@@ -33,4 +34,16 @@ with tab_df_filtered2:
             df2 = pd.read_csv("data/jetfuel_data.csv")
             st.dataframe(dfFilter.filter_dataframe(df2, 'filtered2'))
             st.markdown("""**NOTE**: The function dfFilter.filter_dataframe is not displayed in this code. Please see the GitHub functions for details""")
+
+with tab_df_conditiontree:
+    with st.echo(code_location="below"):
+        df3 = pd.read_csv("data/jetfuel_data.csv")
+        # Basic field configuration from dataframe
+        config = config_from_dataframe(df3)
+        
+        # Condition tree
+        query_string = condition_tree(config)
+        
+        # Filtered dataframe
+        df4 = df3.query(query_string)
 
