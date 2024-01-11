93b08b493a5c758ae831321f54f29c4d10833d6c
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-11 00:42:04 -0600

    Update 5_Data_Visualization_Dropin.py

--- pages/5_Data_Visualization_Dropin.py
+++ pages/5_Data_Visualization_Dropin.py
@@ -9,11 +9,11 @@ import pygwalker as pyg
 
 # Set the title and page overview (assuming you have these functions in your 'ps' module)
 ps.set_title("FlowGenius", "Data Visualization")
-ps.set_page_overview("Overview", "This section shows a Drop in UI for speedy data to Visualization. This can be doen locally within the app, or a file dropped in.")
+ps.set_page_overview("Overview", "This section shows a Drop in UI for speedy data to Visualization. This can be done locally within the app, or a file dropped in.")
 tab_Pygwalker, tab2 = st.tabs(["Drop in Data Visualization UI", "tab2"])
 
 with tab_Pygwalker:
-  with st.expander('Example Dataframe', expanded=True):
+  with st.expander('Example Dataframe', width=1000, expanded=True):
     with st.echo(code_location="below"):
             #create a DataFrame with random values
             df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
