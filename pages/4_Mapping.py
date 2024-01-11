9032cbe822fcd3e02dbdc0a61ba8b9eb0ce3c0b4
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-11 01:44:58 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -14,7 +14,7 @@ ps.set_page_overview("Overview", "This section displays different types of maps,
 tab_map1, tab_map2, tab_map3, tab_map4= st.tabs(["Map 1", "Map 2", "map 3", "Map 4"])
 
 with tab_map1:
-   with exp1 = st.expander("Map 1", expanded=True):
+   with st.expander("Map 1", expanded=True):
     with st.echo(code_location="below"):
         df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
         st.dataframe(df)
