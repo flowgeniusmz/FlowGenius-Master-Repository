1d3de51b650be7c23b88285ebf9625dbc52057c9
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-11 00:38:42 -0600

    Update 5_Data_Visualization_Dropin.py

--- pages/5_Data_Visualization_Dropin.py
+++ pages/5_Data_Visualization_Dropin.py
@@ -2,20 +2,22 @@ from config import pagesetup as ps
 import streamlit as st
 #used to read files (local or brought in)
 import pandas as pd
+import numpy as np
 import streamlit.components.v1 as components
+#data Visualization UI
 import pygwalker as pyg
 
 # Set the title and page overview (assuming you have these functions in your 'ps' module)
 ps.set_title("FlowGenius", "Data Visualization")
-ps.set_page_overview("Overview", "This section displays a basic dataframe using Pandas library.")
-tab_Pygwalker = st.tabs(["Drop in Data Visualization UI"])
+ps.set_page_overview("Overview", "This section shows a Drop in UI for speedy data to Visualization. This can be doen locally within the app, or a file dropped in.")
+tab_Pygwalker, tab2 = st.tabs(["Drop in Data Visualization UI", "tab2"])
+
 with tab_Pygwalker:
   with st.expander('Example Dataframe', expanded=True):
     with st.echo(code_location="below"):
+            #create a DataFrame with random values
             df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
             st.dataframe(df)
-df = pd.read_csv("data/example.csv")
-pyg_html = pyg.walk(df, return_html=True)
-
-components.html(pyg_html, height=1000, scrolling=True)
+            pyg_html = pyg.walk(df, return_html=True)
+            components.html(pyg_html, height=1000, scrolling=True)
         
