ce70edaae9bd9a21c763bfdb1ea509ddb1d27e54
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-11 00:33:01 -0600

    Update 5_Data_Visualization_Dropin.py

--- pages/5_Data_Visualization_Dropin.py
+++ pages/5_Data_Visualization_Dropin.py
@@ -10,7 +10,7 @@ ps.set_title("FlowGenius", "Data Visualization")
 ps.set_page_overview("Overview", "This section displays a basic dataframe using Pandas library.")
 tab_Pygwalker = st.tabs(["Drop in Data Visualization UI"])
 with tab_Pygwalker:
-  with exp1 = st.expander('Example Dataframe'. expanded=True):
+  with st.expander('Example Dataframe', expanded=True):
     with st.echo(code_location="below"):
             df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
             st.dataframe(df)
