ebd676b8db52da88e3b6c7abfa2f9d30535f86d5
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-15 22:25:12 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -11,10 +11,10 @@ import pandas as pd
 # Set the title and page overview (assuming you have these functions in your 'ps' module)
 ps.set_title("FlowGenius", "Mapping")
 ps.set_page_overview("Overview", "This section displays different types of maps, how you can interact with them and other utilities.")
-tab_map1, tab_map2, tab_map3, tab_map4= st.tabs(["Map 1", "Map 2", "map 3", "Map 4"])
+tab_map1, tab_map2, tab_map3, tab_map4= st.tabs(["blocking Map (Pydeck)", "Heatmap ()", "map 3", "Map 4"])
 
 with tab_map1:
-   with st.expander("Map 1", expanded=True):
+   with st.expander("blocking Map (Pydeck)", expanded=True):
     with st.echo(code_location="below"):
         df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
         st.dataframe(df)
