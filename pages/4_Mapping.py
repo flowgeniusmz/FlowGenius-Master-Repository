8fda0029dfb2257fb79e62a4a3aa1597e23ae4bb
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-17 03:26:49 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -39,8 +39,6 @@ with tab_map3:
   with st.echo(code_location="below"):
     m=leafmap.Map(center=[0,0], zoom=2)
     in_geojson ='data/Lat_Long_Data.csv'
-    m.add_geojson(
-      layer_name= "routing lines"
-    )
+    m.add_geojson(layer_name= "routing lines")
     m.to_streamlit()  
 
