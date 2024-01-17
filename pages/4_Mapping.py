bbd770d678dc546ee402a5789085935c40b4b89e
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-16 21:11:12 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -60,3 +60,16 @@ with tab_map2:
 
     # Call the app function to run the app
     app()
+
+with tab_map2:
+   def app():
+      filepath = 'data/Lat_Long_Data.csv'
+      df = pd.read.csv(filepath)
+      m = leafmap.Map()
+      m.add_heatmap(
+         filepath,
+         latitude='latitude',
+         longitude='longitude',
+         value='weight'
+      )
+      m.to_streamlit(height=700)
