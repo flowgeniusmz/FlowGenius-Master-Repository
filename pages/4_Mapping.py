d1184b8a8c18026ab91eadcd1de8691a11d99cea
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-16 23:04:22 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -7,6 +7,7 @@ import folium
 from streamlit_folium import st_folium
 from folium import Popup  # Import the Popup class
 import pandas as pd
+import geopandas
 
 #other mapping packages
 import leafmap.foliumap as leafmap
