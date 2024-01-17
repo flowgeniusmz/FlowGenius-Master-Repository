21434337939bfa1658e0825420cc71b399ebaf4c
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-01-16 22:56:33 -0600

    Update 4_Mapping.py

--- pages/4_Mapping.py
+++ pages/4_Mapping.py
@@ -21,7 +21,7 @@ with tab_map1:
   with st.expander("blocking Map (Pydeck)", expanded=True):
       st.write('yo')
 with tab_map2:
-  def app():
+def app():
    #initialize leafmap
    m = leafmap.Map()
    # Filepath to the local CSV file
